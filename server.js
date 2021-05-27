202a4c551e607bc12b11d733640c7d91c850ff3c
Rishabh Singh <rishabhsinghs90s@gmail.com>
2021-05-28 00:43:55 +0530

    Change db host

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import { handleApiCall, handleImage } from "./controllers/image.js";
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-aerodynamic-15889",
     user: "postgres",
     password: "rishabh123",
     database: "smart-brain",
